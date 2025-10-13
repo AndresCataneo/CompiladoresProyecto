c801efd3ce08f3b4ffee5785ef6dd86020f4d901
Fernando2198 <fernando_frias@ciencias.unam.mx>
2025-10-12 23:12:01 -0600

    Se agrega función para eliminar las cerraduras

--- ProyectoCompiladores/src/AFN.hs
+++ ProyectoCompiladores/src/AFN.hs
@@ -12,7 +12,7 @@ type Transiciones = (Int, Char, [Int])
 
 data AFN = AFN
     {
-        estadoAfn :: [Int],
+        estadosAfn :: [Int],
         alfabetoAfn :: [Char],
         transiciones :: [Transiciones],
         inicialAfn :: Int, 
@@ -42,6 +42,10 @@ epsilonClosure afn estados = Set.toList (close Set.empty estados)
 
 {-
 Función para construir el AFN(Sin transiciones epsilon)
+Reconstruye las transiciones del nuevo AFN considerando estas cerraduras,
+para cada estado y símbolo del alfabeto, se determinan los posibles destinos 
+alcanzables aplicando primero la cerradura, luego las transiciones normales, 
+y finalmente otra closure sobre los estados alcanzados.
 -}
 
 construirAFN :: AFNEp -> [Transiciones] -> [Int] -> AFN
@@ -53,4 +57,39 @@ construirAFN afnEP trans finales' = AFN
      finalesAfn = finales'
      }
 
-eliminarEpsilonTrans :: AFNEp ->
+
+{-
+función quetoma un autómata finito no determinista con transiciones épsilon (AFNEp)
+y devuelve un nuevo autómata sin transiciones épsilon (AFN).
+-}
+
+eliminarEpsilonTrans :: AFNEp -> AFN
+eliminarEpsilonTrans afnEp = construirAFN afnEp nuevaTrans nuevosFinales
+    where
+        closures = [(q, epsilonClosure afnEp [q]) | q <- estados afnEp] --Lista de todas las ε-closures calculados una vez
+
+        -- Función auxiliar que usa lookup
+        lookupCierre q = fromMaybe [] (lookup q closures) -- Busca un estado en la cerradura y valor por defecto si no encuentra el estado
+
+        -- Generar nuevas transiciones sin epsilon
+        nuevaTrans = 
+            [(q, c, destinos q c)
+            | q <- estados afnEp,
+              c <- alfabeto afnEp,
+              not (null (destinos q c))]
+
+        destinos q c =
+            let cierreQ = lookupCierre q -- obtenemos ε-closure(q)
+                movs = [ dst
+                       | p <- cierreQ,
+                         (p', Just x, dsts) <- transiciones afnEp,
+                         p == p', x == c,
+                         dst <- dsts]
+
+            in Set.toList . Set.fromList . concatMap (epsilonClosure afnEp . pure) $ movs
+
+         -- Un estado será final si su cierre contiene algún estado final del original
+        nuevosFinales = 
+            [q
+            |(q, cierreQ) <- closures,
+             any (`elem` final afnEp) cierreQ]
