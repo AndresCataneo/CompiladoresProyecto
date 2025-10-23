54064ead393d0000ebb36058f6197bef025ba831
Maxo <ricardo.max@ciencias.unam.mx>
2025-10-23 14:47:25 -0600

    lexer comentado

--- ProyectoCompiladores/src/Lexer.hs
+++ ProyectoCompiladores/src/Lexer.hs
@@ -1,23 +1,23 @@
-module Lexer() where
+-- module Lexer() where
 
-import MDD
+-- import MDD
 
-data Token = Id String
-                | Entero Int
-                | Asignacion 
-                | OpArit Char
-                | OpBool String
-                | PalabRes String
-                | Delimitadores Char
-                | Espacios String
-                | ComenBloque String
-                | ComenLinea String
-                deriving (Show)
+-- data Token = Id String
+--                 | Entero Int
+--                 | Asignacion 
+--                 | OpArit Char
+--                 | OpBool String
+--                 | PalabRes String
+--                 | Delimitadores Char
+--                 | Espacios String
+--                 | ComenBloque String
+--                 | ComenLinea String
+--                 deriving (Show)
 
-lexer :: String -> [Token]
-lexer _ = []
+-- lexer :: String -> [Token]
+-- lexer _ = []
 
-auxLexer :: 
+-- auxLexer :: 
 
-armaMDD :: RE -> MDD
-armaMDD _ = MDD{}
+-- armaMDD :: RE -> MDD
+-- armaMDD _ = MDD{}
